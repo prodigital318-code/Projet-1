fbe37764c21c81c2bcb23e1f1ecfb6f74e9c5a4b
prodigital318-code <prodigital318@gmail.com>
2025-09-26 09:23:31 +0000

    config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+
+import { createClient } from "@supabase/supabase-js";
+
+// Ton URL et ta clé publique Supabase
+const SUPABASE_URL = "https://hvlqupaphrfrgcajbkyn.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2bHF1cGFwaHJmcmdjYWpia3luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4Nzg0MDIsImV4cCI6MjA3NDQ1NDQwMn0.twsCnIPppre-qf74vOieNEuXd0F-BLDfuMTfzQwmTmA";
+
+// Initialisation du client Supabase
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    autoRefreshToken: true, // garde la session active
+    persistSession: true,   // stocke la session dans localStorage
+  },
+});
